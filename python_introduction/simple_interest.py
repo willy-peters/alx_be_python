57df6e3b2c60f6f581c5ac8fd53b49c3cb03c486
Willy Peter <nguyamuwilly@gmail.com>
2025-07-12 16:59:08 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -6,4 +6,4 @@ Monthly_Savings = monthly_income - monthly_expenses
 Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${Monthly_Savings}.")
-print(f"Projected savings after one year, with interest, is: ${Projected_Savings}.")
+print(f"Projected savings after one year, with interest, is: ${int(Projected_Savings)}.")
