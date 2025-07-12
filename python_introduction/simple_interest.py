6f67cc3c65ecb9603be558413b7532aff55e8cf1
Willy Peter <nguyamuwilly@gmail.com>
2025-07-12 16:33:36 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,9 +3,6 @@ monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 monthly_savings = monthly_income - monthly_expenses
 
-annual_interest_rate = 0.05
-total_annual_savings = monthly_savings * 12
-interest_earned = total_annual_savings * annual_interest_rate
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${monthly_savings:.0f}.")
