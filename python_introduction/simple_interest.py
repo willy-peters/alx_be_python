5569608fc0adddd121a445df6a94eefc49d04d9d
Willy Peter <nguyamuwilly@gmail.com>
2025-08-03 16:42:49 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,14 +1,3 @@
-# All that was needed is a simple python script bruh
-# monthly_income = float(input("Enter your monthly income: "))
-# monthly_expenses = float(input("Enter your total monthly expenses: "))
-
-# Monthly_Savings = monthly_income - monthly_expenses
-
-# Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
-
-# print(f"Your monthly savings are ${Monthly_Savings}.")
-# print(f"Projected savings after one year, with interest, is: $int({Projected_Savings}).")
-
 # This is what was needed
 # Calculating simple interest
 principal = 1000
